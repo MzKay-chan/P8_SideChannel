3bd92eb41f14d8531dfac3b73ea39a2b7c7325cc
Kay <jwcmckay@gmail.com>
2026-02-26 12:32:58 +0100

    Started implementation of analysis class

---
+++ Test1/analysis.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+class analyser
+    def __init__(self, measurements, time):
+    self.measurements = measurements
+    self.time = time
+
+    return
+
+    def analysis(self, measurements, time):
+    return 
+
